9c4ed548256fd90c877b4232dfa99919e947c8e2
Samuel Fringeli <nathan.fluckiger@hotmail.ch>
2018-07-31 12:30:45 +0200

    CO-1922 Debug of conf and header

--- sms_939/models/sms_sender.py
+++ sms_939/models/sms_sender.py
@@ -9,6 +9,7 @@
 #
 ##############################################################################
 from odoo import models, fields, api, tools
+from odoo.tools.config import config
 import httplib
 import base64
 
@@ -33,8 +34,9 @@ class SmsSender(models.TransientModel):
         if not self.partner_id.mobile:
             return False
 
-        username = tools.config.get('939_username')
-        password = tools.config.get('939_password')
+        headers = {}
+        username = config.get('939_username')
+        password = config.get('939_password')
 
         request_server = httplib.HTTPConnection(
             'http://blue.smsbox.ch', 10020, timeout=10)
@@ -42,11 +44,13 @@ class SmsSender(models.TransientModel):
         auth = base64.encodestring('%s:%s' % (username,
                                               password)).replace('\n', '')
 
+        headers['Authorization'] = auth
         for sms in self:
             request = "?receiver=%s&text=%s" % (sms.partner_id.mobile,
                                                 sms.text)
-            request_server.putheader("Authorization", "Basic %s" % auth)
-            request_server.request('GET', '/Blue/sms/xml', request)
+            request_server.request('GET', '/Blue/sms/rest/websend', request,
+                                   headers)
+            print(request_server.getresponse())
             sms.partner_id.message_post(body=sms.text,
                                         subject='Spontaneous SMS',
                                         partner_ids=sms.partner_id,
